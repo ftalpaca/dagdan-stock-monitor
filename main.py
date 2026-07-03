29d88418f27f09b5cb240af3f2f8a6a00bb48a77
ftalpaca <5ybg7mbtkk@privaterelay.appleid.com>
2026-07-03 02:03:53 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import config
+from checker import check_all
+from state import load_state, save_state
+from notifier import notify
+
+DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1522403734993502229/zmtCvR0yzs1mYTkD4d_1BVNFjbp01V_29jnxtOLG5PIIFqZpQaEweznWwZPbzpQP845Q"
+
+
+def run():
+    previous = load_state()
+    current = check_all()
+
+    for retailer, in_stock in current.items():
+        was_in_stock = previous.get(retailer, False)
+
+        if in_stock and not was_in_stock:
+            notify(
+                DISCORD_WEBHOOK,
+                retailer,
+                config.RETAILERS[retailer]["url"]
+            )
+
+    save_state(current)
+
+
+if __name__ == "__main__":
+    run()
